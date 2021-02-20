d2ced5f4233ce97f6b14f059d970cb0881ac3848
abhayMore <abhay7more@gmail.com>
2021-02-20 12:15:21 +0530

    Added completed files and GIF and updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Chaos-Game
+```
 Creating Fractals using Chaos Game, written in C++ and SFML
+```
+
+## Summary
+
+[Chaos Game](https://en.wikipedia.org/wiki/Chaos_game) is a method of creating Fractals, using polygons and an intial random point inside it. A random vertex is chosen and the starting/initial point is moved in that direction in some fraction knowns as distance factor(like 1/3, 1/2 etc). <br /> 
+Taking this new position as current and choosing random vertex again and moving towards it depending on the distance factor, doing this iteratively produces a fractal.
+
+
+## This code focuses on n-flake or Sierpinski n-gon theory
+
+This code shows fractals related to [n-flake](https://en.wikipedia.org/wiki/N-flake) thoery, where n is the number of vertices and if the necessary distance factor is given produces a nice fractal flake.
+
+## How to Run (only for Linux)
+
+Install [SFML](https://www.sfml-dev.org/tutorials/2.5/start-linux.php) <br />
+Install make (command : ```sudo apt install make``` ) <br />
+Run command : ```make F1=main.cpp```
+
+```The program expects two inputs``` <br />
+1. The number of vertices
+2. The distance factor
+
+
+## Output
+
+This is a Sierpinski Triangle, it is an n-flake, where n is 3 and the distance factor is 1/2 or 0.5.
+
+![output](https://raw.githubusercontent.com/abhaMore/Chaos-Game/master/Triangle.gif)
+
+
+
---
+++ main.cpp
@@ -0,0 +1,129 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include <cmath>
+
+class Quadrilateral
+{
+private:
+    sf::CircleShape quad;
+    sf::CircleShape circle;
+
+    int count;
+    float Variable;
+
+    sf::Vector2f CircleXY;
+
+public:
+    Quadrilateral(int VertexNumber, float DistanceFactor);
+
+    void Movement();
+    sf::CircleShape &DrawCircle();
+    sf::CircleShape &DrawQuad();
+    void CircleSetPosition(sf::Vector2f, sf::Vector2f);
+};
+Quadrilateral::Quadrilateral(int VertexNumber, float DistanceFactor) : CircleXY(300.0f, 500.0f), count(VertexNumber), Variable(DistanceFactor)
+{
+    srand(time(0));
+    quad.setRadius(300);
+    quad.setPointCount(count);
+
+    quad.setOutlineThickness(0.8);
+    quad.setFillColor(sf::Color::Transparent);
+
+    circle.setRadius(1);
+    circle.setPosition(CircleXY);
+    circle.setFillColor(sf::Color::Magenta);
+}
+
+void Quadrilateral::CircleSetPosition(sf::Vector2f TRI, sf::Vector2f CIR)
+{
+    float PositionX = (TRI.x * (1 - Variable) + CIR.x * Variable);
+    float PositionY = (TRI.y * (1 - Variable) + CIR.y * Variable);
+
+    circle.setPosition(PositionX, PositionY);
+}
+
+void Quadrilateral::Movement()
+{
+    int random = rand() % (count * 2 ) + 1;
+
+    if (count == 4)
+    {
+        if(random < 5)
+        {
+            CircleSetPosition(quad.getPoint((random - 1)), CircleXY);
+        }
+        else if (random == 5)
+        {
+            sf::Vector2f temp((quad.getPoint(0).x + quad.getPoint(1).x) / 2, (quad.getPoint(0).y + quad.getPoint(1).y) / 2);
+            CircleSetPosition(temp, CircleXY);
+        }
+        else if (random == 6)
+        {
+            sf::Vector2f temp((quad.getPoint(1).x + quad.getPoint(2).x) / 2, (quad.getPoint(1).y + quad.getPoint(2).y) / 2);
+            CircleSetPosition(temp, CircleXY);
+        }
+        else if (random == 7)
+        {
+            sf::Vector2f temp((quad.getPoint(0).x + quad.getPoint(3).x) / 2, (quad.getPoint(0).y + quad.getPoint(3).y) / 2);
+            CircleSetPosition(temp, CircleXY);
+        }
+        else if (random == 8)
+        {
+            sf::Vector2f temp((quad.getPoint(2).x + quad.getPoint(3).x) / 2, (quad.getPoint(2).y + quad.getPoint(3).y) / 2);
+            CircleSetPosition(temp, CircleXY);
+        }
+    }
+    else
+    {
+        if (random % 2 == 0)
+            CircleSetPosition(quad.getPoint((random - 2) / 2), CircleXY);
+        else
+            CircleSetPosition(quad.getPoint((random - 1) / 2), CircleXY);
+    }
+    CircleXY = circle.getPosition();
+}
+
+sf::CircleShape &Quadrilateral::DrawCircle()
+{
+    return circle;
+}
+
+sf::CircleShape &Quadrilateral::DrawQuad()
+{
+    return quad;
+}
+
+int main()
+{
+    std::cout << "Enter Vertex Number and Distance factor" << std::endl;
+    int VertexNumber;
+    float DistanceFactor;
+    std::cin >> VertexNumber >> DistanceFactor;
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ChaosGame");
+    window.setVerticalSyncEnabled(true);
+
+   
+    //window.setFramerateLimit(120);
+
+    Quadrilateral Shape(VertexNumber, DistanceFactor);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        Shape.Movement();
+	
+        //window.clear();
+
+	window.draw(Shape.DrawQuad());
+        window.draw(Shape.DrawCircle());
+        	
+        window.display();
+    }
+}
